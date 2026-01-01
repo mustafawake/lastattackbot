de0723df6f22af07ea612dfbf84ca28035baa3cd
mustafawake <birselhotmail123@gmail.com>
2026-01-01 16:17:50 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot aktif: ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("pong 🏓");
+  }
+});
+
+client.login(process.env.TOKEN);
